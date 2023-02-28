9779029855d7de60fcd4a8610516bd2d81459016
Alex Romanovskii <alex.romanovskii@resolver.com>
2023-02-27 19:41:52 -0500

    Add more Log examples

--- log/main.go
+++ log/main.go
@@ -15,7 +15,7 @@ func main() {
 	appCfg := Config{}
 	envconfig.MustProcess("MYAPP", &appCfg)
 
-	cfg := zap.Config{
+	logConfig := zap.Config{
 		Level:            zap.NewAtomicLevelAt(appCfg.Level),
 		DisableCaller:    true,
 		Development:      true,
@@ -24,17 +24,41 @@ func main() {
 		ErrorOutputPaths: []string{"stderr"},
 		EncoderConfig:    zap.NewDevelopmentEncoderConfig(),
 	}
+	logConfig.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder // make output with colors!
 
-	logger := zap.Must(cfg.Build()).Sugar()
+	logger := zap.Must(logConfig.Build()).Sugar()
 
 	logger.Info("Started")
 	logger.Debug("Debug mode enabled")
 
-	logger.With("count", 1).Info("One banana")
-	logger.Desugar().With(zap.Int("count", 1)).Info("Two banana, no sugar")
+	// ---
+	// logger.With("count", 1).Info("One banana")
+	// logger.Desugar().With(zap.Int("count", 1)).Info("Two banana, no sugar")
+	// logger.Infow("from infow", "count", 1, "name", "banana")
+
+	// ---
+	// parseResult, err := strconv.ParseBool("истина")
+	// if err != nil {
+	// 	logger.Errorw("error while parsing", "error", err)
+	// }
+	// logger.Info("parse result", "result", parseResult)
+
+	// ---
+	// requestLogger := logger.With("request_id", "12345")
+	// processRequest(requestLogger)
+
+	// ---
+	// configuredLogger := logger.WithOptions(zap.AddStacktrace(zapcore.InfoLevel))
+	// configuredLogger.Info("message example")
 
 }
 
+// func processRequest(logger *zap.SugaredLogger) {
+// 	logger.Info("Processing started")
+// 	logger.Error("Processing finished", "status", "error")
+// }
+
 // To show:
 // 1. init, config
-// 2. fields (sugar, desugar)
+// 2. fields (sugar, desugar, infow)
+// 3. logger inheritance
